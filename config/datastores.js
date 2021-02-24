5c1c0ebe80100a16990e6e6cbf5e3d3f610e36d4
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-02-24 17:28:05 +0200

    connected a database

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root1:1234@localhost:3306/VideoClub',
+    url: 'mysql://root1:1234@localhost:3306/video_club',
 
   },
 
