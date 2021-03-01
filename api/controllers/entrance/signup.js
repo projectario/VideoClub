3905a403ca52aac0adf174e8f4e8a85459567e15
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-03-01 17:48:15 +0200

    re-wrote the signup code

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -23,19 +23,20 @@ module.exports = {
 
 
   fn: async function ({ firstName, lastName, email, password, confirmPassword }) {
-    let isUser = await User.findOne({ email: email })
-    console.log(email)
-    if (isUser) {
-      return this.res.alreadyExists("<h2> Email already in use! </h2>")
-    }
-    else if (password == confirmPassword) {
-      let user = await User.create({ firstName: firstName, lastName: lastName, email: email, password: password })
-      this.res.redirect('/');
-    }
-    else {
-      this.res.status (406);
-      return this.res.view('entrance/view-signup')
-    }
+    // let isUser = await User.findOne({ email: email })
+    // console.log(email)
+    // if (isUser) {
+    //   return this.res.alreadyExists("<h2> Email already in use! </h2>")
+    // }
+    // if (password == confirmPassword) {
+    var user = await User.create({ firstName: firstName, lastName: lastName, email: email, password: password })
+    this.res.redirect('/');
+    return {};
+    // }
+    // else {
+    //   this.res.status (406);
+    //   return this.res.view('entrance/view-signup')
+    // }
   }
 
 
