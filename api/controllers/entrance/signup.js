ee051bbf3982c50ccbb9f4fe0ece3e70e2c8c0af
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-03-01 17:28:26 +0200

    re-wrote signup

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -1,4 +1,3 @@
-const User = require("../../models/User");
 
 module.exports = {
 
@@ -23,18 +22,20 @@ module.exports = {
   },
 
 
-  fn: async function ({ firstName, lastName, email, password }) {
-    let isUser = await User.findOne({ email })
+  fn: async function ({ firstName, lastName, email, password, confirmPassword }) {
+    let isUser = await User.findOne({ email: email })
     console.log(email)
-    if (isUser) return;
+    if (isUser) {
+      return this.res.alreadyExists("<h2> Email already in use! </h2>")
+    }
     else if (password == confirmPassword) {
-      let newUser = await User.create({ firstName, lastName, email, password })
+      let user = await User.create({ firstName: firstName, lastName: lastName, email: email, password: password })
       this.res.redirect('/');
     }
-    else { }
-    // All done.
-    this.res.status(406)
-    return this.res.view('entrance/view-signup')
+    else {
+      this.res.status (406);
+      return this.res.view('entrance/view-signup')
+    }
   }
 
 
