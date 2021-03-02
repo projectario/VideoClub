b9a686554c1452df2e0e5cf962f3e79e4fbcb6dc
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-03-02 19:11:00 +0200

    fixed db connection

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://user:1234@localhost:3306/movieclub',
+    url: 'mysql://user:1234@ra1.anystream.eu:1057/movieclub',
   },
 
 };
