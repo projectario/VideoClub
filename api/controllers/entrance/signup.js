671f756dc74181f3b66e2fbd9de2f921105cf32b
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-03-02 19:09:41 +0200

    signup now inserts users and validates

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -18,27 +18,22 @@ module.exports = {
   },
 
 
-  exits: {
-
-  },
+  exits: {},
 
 
-  fn: async function (inputs) {
-    let isUser = await User.findOne({ email: inputs.email })
-    console.log(email)
-    if (isUser) {
-      return this.res.alreadyExists("<h2> Email already in use! </h2>")
-    }
+  fn: async function ({firstName, lastName, email, password, confirmPassword, isAdult}) {
+    console.log(firstName, lastName, email, password, confirmPassword, isAdult);
+    var isUser = await User.findOne({email: email});
+    if (isUser) this.res.alreadyExists("<h2> Email already in use! </h2>");
     else {
-      if (inputs.password == inputs.confirmPassword) {
-        var user = await User.create({ firstName: inputs.firstName, lastName: inputs.lastName, email: inputs.email, password: inputs.password })
-        this.res.redirect('/');
-      } else {
-        this.res.status (406);
-        return this.res.view('entrance/view-signup')
+      if (password == confirmPassword) {
+        await User.create({firstName: firstName, lastName: lastName, email: email, password: password, confirmPassword: confirmPassword})
+        this.res.redirect('/')
       }
-    }   
+      else {
+        return this.res.passwordsDontMatch("<h1>Passwords not match!!!</h1>")
+      }
+    }
   }
-
 
 };
