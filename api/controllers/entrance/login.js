0972b60701b0de638447a681b4ac88a4cf9f0f31
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-03-04 12:58:31 +0200

    template login action

---
+++ api/controllers/entrance/login.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Login',
+
+
+  description: 'Login to the App.',
+
+
+  inputs: {
+    
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
