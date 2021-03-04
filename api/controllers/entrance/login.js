d82dc3a104a20f779fc58863d4fb53603c1a3b3e
BanNektarios <63299482+BanNektarios@users.noreply.github.com>
2021-03-04 20:18:42 +0200

    login action/controller

--- api/controllers/entrance/login.js
+++ api/controllers/entrance/login.js
@@ -8,19 +8,35 @@ module.exports = {
 
 
   inputs: {
-    
+    email: { type: 'string', required: true },
+    password: { type: 'string', required: true },
   },
 
 
   exits: {
-
+    success: {
+      viewTemplatePath: 'pages/entrance/login'
+    }
   },
 
 
-  fn: async function (inputs) {
+  fn: async function ({ email, password }) {
+    console.log(email, password)
+    let isUser = await User.findOne({ email: email.toLowerCase() });
+    if (!isUser) return this.res.emailDoesNotExist('<h1>We dont know anyone that goes by that name! Try Again!</h1>'); // custom response about not matching email?
 
-    // All done.
-    return;
+    else if (isUser) {
+      let user = await User.findOne({ email: email })
+      sails.log(user.password)
+
+      if (user.password === password) {
+        sails.log("LOGGED IN!!")
+        this.res.redirect('/');
+      }
+      else {
+        return this.res.passwordsDontMatch("<h1>Passwords not match!!!</h1>")
+      }
+    }
 
   }
 
