1266585bacc0f10696238c111d3f31adf769c4c9
GrigorisPs <73849201+GrigorisPs@users.noreply.github.com>
2021-03-10 21:00:22 +0200

    trying to fix kidsmovies

--- api/controllers/content/kidsmovies.js
+++ api/controllers/content/kidsmovies.js
@@ -15,12 +15,29 @@ module.exports = {
 
 
   fn: async function () {
-    if (this.req.session.userId == undefined) throw { redirect: '/login' }
-    let listOfKidsMovies = await Film.find({ properForKids: 1 });
-    sails.log(listOfKidsMovies)
-    // Respond with view.
 
-    return { listOfKidsMovies };
+    // Respond with view.
+    let sessionUserId = this.req.session.userId;
+
+    if (sessionUserId == undefined) throw { redirect: '/login' }
+
+    let user = await User.findOne({ id: sessionUserId })
+    // sails.log(user)
+    if (user.isKid) throw { redirect: '/kidsmovies' }
+
+    // let listOfMovies = await Film.find();
+    let listOfKidsMovies;
+    if (genre == 'All') {
+      listOfKidsMovies = await Film.find({ properForKids: 1 });
+
+    } else {
+      listOfKidsMovies = await Film.find({ genre: genre, properForKids: 1 })
+
+    }
+    // All done.
+    return { listOfKidsMovies, genre };
+
+
 
   }
 
