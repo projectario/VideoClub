9e66606c11c4e9faf1051fc628157996afb4fd5e
GrigorisPs <73849201+GrigorisPs@users.noreply.github.com>
2021-03-10 21:18:27 +0200

    Update kidsmovies.js

--- api/controllers/content/kidsmovies.js
+++ api/controllers/content/kidsmovies.js
@@ -33,8 +33,8 @@ module.exports = {
     // let user = await User.findOne({ id: sessionUserId })
     // sails.log(user)
     // if (user.isKid) throw { redirect: '/kidsmovies' }
-
     // let listOfMovies = await Film.find();
+
     let listOfKidsMovies;
     if (genre == 'All') {
       listOfKidsMovies = await Film.find({ properForKids: 1 });
@@ -45,26 +45,7 @@ module.exports = {
     }
     // All done.
     return { listOfKidsMovies, genre };
-    // ............................................
-    // let sessionUserId = this.req.session.userId;
 
-    // if (sessionUserId == undefined) throw { redirect: '/login' }
-
-    // let user = await User.findOne({ id: sessionUserId })
-    // // sails.log(user)
-    // // if (user.isKid) throw { redirect: '/kidsmovies' }
-
-    // // let listOfMovies = await Film.find();
-    // let listOfMovies;
-    // if (genre == 'All') {
-    //   listOfMovies = await Film.find({ properForKids: 1 });
-
-    // } else {
-    //   listOfMovies = await Film.find({ genre: genre, properForKids: 1 })
-
-    // }
-    // // All done.
-    // return { listOfMovies, genre };
 
 
 
