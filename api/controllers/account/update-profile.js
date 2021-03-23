6bd2c74cc43acc6a9568bf95563f816dd42e9dce
GrigorisPs <73849201+GrigorisPs@users.noreply.github.com>
2021-03-23 22:26:39 +0200

    you can change both email and password

--- api/controllers/account/update-profile.js
+++ api/controllers/account/update-profile.js
@@ -13,7 +13,7 @@ module.exports = {
         lastName: { type: 'string' },
         email: { type: 'string' },
         password: { type: 'string' },
-        confirmPassword: { type: 'string', required: true },
+        // confirmPassword: { type: 'string', required: true },
     },
     exits: {
         redirect: {
@@ -32,39 +32,35 @@ module.exports = {
         let res = this.res
         let userId = this.req.session.userId;
         let user = await User.findOne({ id: userId });
+        // sails.log(user)
 
 
-        if (!(password === confirmPassword)) {
-            throw { redirect: '/account/profile' }
-        } else {
-            var newPassword = await bcrypt.hash(password, 12)
-            await User.updateOne({ id: userId })
-                .set({ password: newPassword });
-            delete this.req.session.userId;
-            throw { redirect: '/login' };
-        }
-
-
-        // if (newPassword === confirmPassword) {
+        // if (!(password === confirmPassword)) {
+        //     throw { redirect: '/account/profile' }
+        // } else {
         //     var newPassword = await bcrypt.hash(password, 12)
-
         //     await User.updateOne({ id: userId })
-        //             .set({ password: newPassword });
-        // }
-        // await User.updateOne({ id: userId })
         //         .set({ password: newPassword });
+        //     delete this.req.session.userId;
+        //     throw { redirect: '/login' };
+        // }
 
         //update email
 
-        // var newEmailAddress = email;
+        var newEmail = email
+        sails.log(newEmail)
+        var validEmail = await User.findOne({ email: newEmail })
+        sails.log(validEmail)
+        // sails.log(validEmail.email)
+        if (validEmail !== undefined) throw { problem: '<h2> Email already in use! </h2>' }
+        else {
+            await User.updateOne({ id: userId })
+                .set({ email: newEmail });
+            delete this.req.session.userId;
+            throw { redirect: '/login' };
 
-        // if (newEmailAddress === User.find({ email: newEmailAddress })) {
-        //     throw { problem: 'Email Already in use' }
-        // } else {
-        //     await User.updateOne({ id: userId })
-        //         .set({ email: newEmailAddress });
-        //     return res.ok();
-        // }
+        }
+
 
 
 
